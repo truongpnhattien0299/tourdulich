8b7c958e080d300b41caf454dc078b74f0fb4d47
truongpnhattien0299 <truongpnhattien0299@gmail.com>
2020-11-08 22:33:35 +0700

    test

--- config/app.php
+++ config/app.php
@@ -219,6 +219,7 @@ return [
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
+        'RateLimiter' => Illuminate\Support\Facades\RateLimiter::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
