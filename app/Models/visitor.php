f21231eb9288bab61279f446fb3cfa4b36ef1fb5
unknown <tinhphan31199@gmail..com>
2020-11-11 15:56:43 +0700

    tp

---
+++ app/Models/visitor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class visitor extends Model
+{
+    use HasFactory;
+    protected $table = "tour_nguoidi";
+    protected $primaryKey="nguoidi_id";
+    public $timestamps = false;
+
+    public function doantour()
+    {
+        return $this->belongsTo('App\Models\group', 'doan_id', 'doan_id');
+    }
+
+}
