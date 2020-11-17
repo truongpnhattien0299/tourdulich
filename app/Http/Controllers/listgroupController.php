882349bebd8ba8dd871eaa8d17b15d2a126cba97
unknown <tinhphan31199@gmail..com>
2020-11-17 14:15:21 +0700

    tp

--- app/Http/Controllers/listgroupController.php
+++ app/Http/Controllers/listgroupController.php
@@ -31,36 +31,47 @@ class listgroupController extends Controller
 
     public function postAddlistgroup(Request $request)
     {
-        try{
-            $listgroup = new listgroup;
-            $listgroup->doan_id = $request->group;
-            $arr= $request->ListCustomer;
-            $arr1=[];
-            for($i = 0; $i<count($arr) ;$i++){
-                $obj = (object) [];
-                $group = group::find($request->group);
-                $obj->id=$arr[$i];
-                $obj->ngaydi=$group->doan_ngaydi;
-                $obj->ngayve=$group->doan_ngayve;
-                array_push($arr1, $obj);
+        $listgroup = listgroup::where("doan_id",$request->group)->get()->count();
+        if($listgroup > 0){
+            return back()->withError('Can\'t create new listgroup. Because group has been create')->withInput();
+        }else{
+            try{
+                if($request->ListCustomer == null){
+                    return back()->withError('Can\'t create new listgroup.List customer is null')->withInput();
+                }
+                if($request->Listemployee == null){
+                    return back()->withError('Can\'t create new listgroup. List employee is null')->withInput();
+                }
+                $listgroup = new listgroup;
+                $listgroup->doan_id = $request->group;
+                $arr= $request->ListCustomer;
+                $arr1=[];
+                for($i = 0; $i<count($arr) ;$i++){
+                    $obj = (object) [];
+                    $group = group::find($request->group);
+                    $obj->id=$arr[$i];
+                    $obj->ngaydi=$group->doan_ngaydi;
+                    $obj->ngayve=$group->doan_ngayve;
+                    array_push($arr1, $obj);
+                }
+                $json = json_encode($arr1);
+                $listgroup->nguoidi_dskhach = $json;
+                $arr2= $request->Listemployee;
+                $arr3=[];
+                for($i = 0; $i<count($arr2) ;$i++){
+                    $obj = (object) [];
+                    $group = group::find($request->group);
+                    $obj->id=$arr2[$i];
+                    $obj->ngaydi=$group->doan_ngaydi;
+                    $obj->ngayve=$group->doan_ngayve;
+                    array_push($arr3, $obj);
+                }
+                $json1 = json_encode($arr3);
+                $listgroup->nguoidi_dsnhanvien = $json1;
+                $listgroup->save();
+            }catch(QueryException $e){
+                return back()->withError('Can\'t create new listgroup. Because form incomplete')->withInput();
             }
-            $json = json_encode($arr1);
-            $listgroup->nguoidi_dskhach = $json;
-            $arr2= $request->Listemployee;
-            $arr3=[];
-            for($i = 0; $i<count($arr2) ;$i++){
-                $obj = (object) [];
-                $group = group::find($request->group);
-                $obj->id=$arr2[$i];
-                $obj->ngaydi=$group->doan_ngaydi;
-                $obj->ngayve=$group->doan_ngayve;
-                array_push($arr3, $obj);
-            }
-            $json1 = json_encode($arr3);
-            $listgroup->nguoidi_dsnhanvien = $json1;
-            $listgroup->save();
-        }catch(QueryException $e){
-            return back()->withError('Can\'t create new listgroup. Because form incomplete')->withInput();
         }
         return redirect('Listgroup/listlgrp');
     }
